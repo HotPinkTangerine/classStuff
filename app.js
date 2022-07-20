16ab86388a65d43429caca579970f07ec348a484
Tobey Harrill <hotpinktangerine@gmail.com>
2022-07-19 17:16:11 -0700

    The Button Welcome

---
+++ app.js
@@ -0,0 +1,16 @@
+let play = document.querySelector(".playButton")
+let message = document.querySelector(".message")
+let Intro = document.querySelector(".username-input")
+//var int = setInterval("doSomething()", 5000 );
+
+play.onclick = function(){
+    message.textContent = "Welcome " + Intro.value + "!!!!"
+    //(function(){
+
+        //doSomething();
+     
+        //setTimeout(arguments.callee, 5000);
+     
+     //})()
+    message.textContent = "sup"
+}
