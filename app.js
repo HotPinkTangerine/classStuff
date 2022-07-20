00da5e437cfd276e00b040ba7ef390be08b421aa
Tobey Harrill <hotpinktangerine@gmail.com>
2022-07-20 16:20:28 -0700

    second screen background

--- app.js
+++ app.js
@@ -2,12 +2,17 @@ let play = document.querySelector(".playButton")
 let message = document.querySelector(".message")
 let Intro = document.querySelector(".username-input")
 
+
 play.onclick = function(){
     message.textContent = "Welcome " + Intro.value + "!!!!"
     const NEXT = document.querySelector(".NEXT")
     NEXT.style["display"] = "inline"
     NEXT.onclick = function(){
-        window.location.href = "nextPage.html";
+        const screen1 = document.querySelector(".screen1")
+        screen1.style["display"] = "none"
+        const screen2 = document.querySelector(".screen2")
+        screen2.style["display"] = "block"
+
     }
 }
 
