c427b3cbf7d38660861f30a45e9ccb48e86c5fca
Tobey Harrill <hotpinktangerine@gmail.com>
2022-07-21 16:46:31 -0700

    circle

--- app.js
+++ app.js
@@ -1,8 +1,13 @@
-let play = document.querySelector(".playButton")
-let message = document.querySelector(".message")
-let Intro = document.querySelector(".username-input")
+const play = document.querySelector(".playButton")
+const message = document.querySelector(".message")
+const Intro = document.querySelector(".username-input")
+const player = document.querySelector('.player')
 
 play.onclick = function(){
+    function startGame() {
+        myGamePiece = new component(30, 30, "cracked.png", 10, 120, "image");
+        myGameArea.start();
+      }
     message.textContent = "Welcome " + Intro.value + "!!!!"
     const NEXT = document.querySelector(".NEXT")
     NEXT.style["display"] = "inline"
@@ -11,5 +16,6 @@ play.onclick = function(){
         screen1.style["display"] = "none"
         const screen2 = document.querySelector(".screen2")
         screen2.style["display"] = "block"
-    }
-}
+        Intro.style['display'] = "block"
+        Intro.style.top = player.style.top
+    }}
