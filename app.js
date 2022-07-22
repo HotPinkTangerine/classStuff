d9205820d363446b2188f0e28390406f4b547727
Tobey Harrill <hotpinktangerine@gmail.com>
2022-07-21 20:29:49 -0700

    we got the username next to the ball

--- app.js
+++ app.js
@@ -1,21 +1,25 @@
 const play = document.querySelector(".playButton")
 const message = document.querySelector(".message")
-const Intro = document.querySelector(".username-input")
+const username = document.querySelector(".username-input")
 const player = document.querySelector('.player')
+const message2 = document.querySelector('.message2')
+const NEXT = document.querySelector('.NEXT')
+// console.log('i am your father')
 
 play.onclick = function(){
-    function startGame() {
-        myGamePiece = new component(30, 30, "cracked.png", 10, 120, "image");
-        myGameArea.start();
-      }
-    message.textContent = "Welcome " + Intro.value + "!!!!"
+    message.textContent = "Welcome " + username.value + "!!!!"
     const NEXT = document.querySelector(".NEXT")
     NEXT.style["display"] = "inline"
+    // console.log('next button shows up')
+
     NEXT.onclick = function(){
         const screen1 = document.querySelector(".screen1")
         screen1.style["display"] = "none"
         const screen2 = document.querySelector(".screen2")
         screen2.style["display"] = "block"
-        Intro.style['display'] = "block"
-        Intro.style.top = player.style.top
+        // console.log(username.value)
+        message2.textContent = username.value
+        // player.style.bottom = 'auto'
+        message2.style.bottom = player.style.bottom
+        // console.log(alert(player.style.bottom))
     }}
