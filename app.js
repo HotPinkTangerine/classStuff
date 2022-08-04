4b66e820c8855ee156e5dc5d0c542343754738e4
Tobey Harrill <hotpinktangerine@gmail.com>
2022-08-04 09:09:35 -0700

    10msf

--- app.js
+++ app.js
@@ -149,7 +149,7 @@ let p2v2 = 0
     
     const sendPosition = ()=>{
         holler.appInstance.notifyClients(user.name + "|" + player.style.top + "|" + player.style.left + "|" + velocity  + "|" + velocity2 + "|" + username.value)
-        setTimeout(sendPosition,100)
+        setTimeout(sendPosition,10)
     }
     sendPosition()
 
