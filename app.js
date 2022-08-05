cccd3ab4333454db6d6feb66212b5160abe13572
Tobey Harrill <hotpinktangerine@gmail.com>
2022-08-05 11:45:10 -0700

    lifes saved by james

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 holler.onLoad(()=>{
     holler.me((user)=>{
 
-console.log("my name: " + user.name)
+        console.log("my name: " + user.name)
 
         function makePlayer(){
             let PLAYER = document.createElement("IMG");
@@ -14,8 +14,8 @@ console.log("my name: " + user.name)
             document.body.appendChild(PLAYER)
             return PLAYER
         }
-const userNames = []
-let players = []
+        const userNames = []
+        let players = []
         holler.onClientEvent(event=>{
             console.log('yolo')
             const project = event.split("|")
@@ -56,144 +56,144 @@ let players = []
                 }
             }
         })
-         
-    const play = document.querySelector(".playButton")
-    const message = document.querySelector(".message")
-    const username = document.querySelector(".username-input")
-    let player = document.querySelector('.player')
-    const message2 = document.querySelector('.message2')
-    const NEXT = document.querySelector('.NEXT')
-    const sendMessageResponseArea = document.querySelector('.response-to-send-message')
-    const sendMessageResponseArea2 = document.querySelector('.response-to-send-message2')
+                
+        const play = document.querySelector(".playButton")
+        const message = document.querySelector(".message")
+        const username = document.querySelector(".username-input")
+        let player = document.querySelector('.player')
+        const message2 = document.querySelector('.message2')
+        const NEXT = document.querySelector('.NEXT')
+        const sendMessageResponseArea = document.querySelector('.response-to-send-message')
+        const sendMessageResponseArea2 = document.querySelector('.response-to-send-message2')
 
-    play.onclick = function(){
-        message.textContent = "Welcome " + username.value + "!!!!"
-        const NEXT = document.querySelector(".NEXT")
-        NEXT.style["display"] = "block"
-        
-        NEXT.onclick = function(){
-            userNames.push(user.name)
-            const screen1 = document.querySelector(".screen1")
-            screen1.style["display"] = "none"
-            let screen2 = document.querySelector(".screen2")
-            screen2.style["display"] = "block"
-            message2.textContent = username.value
-            player = makePlayer()
+        play.onclick = function(){
+            message.textContent = "Welcome " + username.value + "!!!!"
+            const NEXT = document.querySelector(".NEXT")
+            NEXT.style["display"] = "block"
+            
+            NEXT.onclick = function(){
+                userNames.push(user.name)
+                const screen1 = document.querySelector(".screen1")
+                screen1.style["display"] = "none"
+                let screen2 = document.querySelector(".screen2")
+                screen2.style["display"] = "block"
+                message2.textContent = username.value
+                player = makePlayer()
+            }
+
+            //left and right
+
+            let position = window.visualViewport.width / 2
+            let velocity = 0
+
+
+            let showVelocityWarnings = ()=>{
+                let velocityMessage
+                let velocityMessageColor
+
+                if(velocity>5 || velocity < -5){
+                    velocityMessage = ""
+                }else if(velocity>2 || velocity < -2){
+                    velocityMessage = ""
+                }else{
+                    velocityMessage = ""
+                    velocityMessageColor = "none"
+                }
+                sendMessageResponseArea.textContent = velocityMessage
+                sendMessageResponseArea.style["background"] = velocityMessageColor
+            }
+
+            document.onkeydown = (keyEvent)=>{
+                console.log("Keypressed: " + keyEvent.key)
+                switch(keyEvent.key){
+                    case "a": 
+                    case "ArrowLeft":
+                        velocity = velocity -1
+                        break;
+                    case "d": 
+                    case "ArrowRight":
+                        velocity = velocity +1
+                        break;
+                    case "w": 
+                    case "ArrowUp":
+                        velocity2 = velocity2 -1
+                        break;
+                    case "s":
+                    case "ArrowDown":
+                        velocity2 = velocity2 +1
+                        break;
+                }
+                console.log("Current velocity is", velocity)
+
+                showVelocityWarnings()
+            }
+
+            const doNextFrame = ()=>{
+                position = position + velocity
+                setTimeout(doNextFrame)
+                if (position<1282 - 88 && position>0){
+                    player.style.left = position + "px"
+                }else {
+                    velocity = 0
+                }
+            }
+
+            doNextFrame()
+
+            //up and down
+
+            let position2 = window.visualViewport.height / 2
+            let velocity2 = 0
+
+
+            let showVelocityWarnings2 = ()=>{
+                let velocityMessage2
+                let velocityMessageColor2
+
+                if(velocity2>5 || velocity2 < -5){
+                    velocityMessage2 = ""
+                }else if(velocity2>2 || velocity2 < -2){
+                    velocityMessage2 = ""
+                }else{
+                    velocityMessage2 = ""
+                    velocityMessageColor2 = "none"
+                }
+                sendMessageResponseArea2.textContent = velocityMessage2
+                sendMessageResponseArea2.style["background"] = velocityMessageColor2
+            }
+
+            showVelocityWarnings2()
+
+            const doNextFrame2 = ()=>{
+                position2 = position2 + velocity2
+                if (position2<542 && position2>0){
+                    player.style.top = position2 + "px"
+                }else {
+                    velocity2 = 0
+                }
+                
+                setTimeout(doNextFrame2)
+            }
+            doNextFrame2()
+
+            const followplayer = ()=>{
+                message2.style.top = parseInt(player.style.top) - 300
+                message2.style.left = parseInt(player.style.left) - 648
+                setTimeout(followplayer, 2)
+            }
+            followplayer()
+
+
+            const sendPosition = ()=>{
+
+                holler.appInstance.notifyClients(user.name + "|" + player.style.top + "|" + player.style.left + "|" + velocity  + "|" + velocity2 + "|" + username.value + "|" + message2)
+                setTimeout(sendPosition,10)
+
+                console.log('user.name is :' + user.name)
+
+            }
+            sendPosition()
         }
-    
-    //left and right
-    
-    let position = window.visualViewport.width / 2
-    let velocity = 0
-
-
-    let showVelocityWarnings = ()=>{
-        let velocityMessage
-        let velocityMessageColor
-
-        if(velocity>5 || velocity < -5){
-            velocityMessage = ""
-        }else if(velocity>2 || velocity < -2){
-            velocityMessage = ""
-        }else{
-            velocityMessage = ""
-            velocityMessageColor = "none"
-        }
-        sendMessageResponseArea.textContent = velocityMessage
-        sendMessageResponseArea.style["background"] = velocityMessageColor
-    }
-
-    document.onkeydown = (keyEvent)=>{
-        console.log("Keypressed: " + keyEvent.key)
-        switch(keyEvent.key){
-            case "a": 
-            case "ArrowLeft":
-                velocity = velocity -1
-                break;
-            case "d": 
-            case "ArrowRight":
-                velocity = velocity +1
-                break;
-            case "w": 
-            case "ArrowUp":
-                velocity2 = velocity2 -1
-                break;
-            case "s":
-            case "ArrowDown":
-                velocity2 = velocity2 +1
-                break;
-        }
-        console.log("Current velocity is", velocity)
-
-        showVelocityWarnings()
-    }
-
-    const doNextFrame = ()=>{
-        position = position + velocity
-        setTimeout(doNextFrame)
-        if (position<1282 - 88 && position>0){
-            player.style.left = position + "px"
-        }else {
-            velocity = 0
-        }
-    }
-
-    doNextFrame()
-
-    //up and down
-
-    let position2 = window.visualViewport.height / 2
-    let velocity2 = 0
-
-
-    let showVelocityWarnings2 = ()=>{
-        let velocityMessage2
-        let velocityMessageColor2
-
-        if(velocity2>5 || velocity2 < -5){
-            velocityMessage2 = ""
-        }else if(velocity2>2 || velocity2 < -2){
-            velocityMessage2 = ""
-        }else{
-            velocityMessage2 = ""
-            velocityMessageColor2 = "none"
-        }
-        sendMessageResponseArea2.textContent = velocityMessage2
-        sendMessageResponseArea2.style["background"] = velocityMessageColor2
-    }
-
-        showVelocityWarnings2()
-
-    const doNextFrame2 = ()=>{
-        position2 = position2 + velocity2
-        if (position2<542 && position2>0){
-            player.style.top = position2 + "px"
-        }else {
-            velocity2 = 0
-        }
-        
-        setTimeout(doNextFrame2)
-    }
-    doNextFrame2()
-
-    const followplayer = ()=>{
-        message2.style.top = parseInt(player.style.top) - 300
-        message2.style.left = parseInt(player.style.left) - 648
-        setTimeout(followplayer, 2)
-    }
-    followplayer()
-   
-    
-    const sendPosition = ()=>{
-
-        holler.appInstance.notifyClients(user.name + "|" + player.style.top + "|" + player.style.left + "|" + velocity  + "|" + velocity2 + "|" + username.value + "|" + message2)
-        setTimeout(sendPosition,10)
-
-        console.log('user.name is :' + user.name)
-
-    }
-    sendPosition()
-    }
 
     })
 })
